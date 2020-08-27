09a908831b3a611923f1c18cdbc3392a343ae562
Gavin Schriver <alexanderschriver@gmail.com>
2020-08-27 00:50:26 -0500

    goin userfishin in savefriendbutton module

--- src/scripts/friends/NewFriendEntry.js
+++ src/scripts/friends/NewFriendEntry.js
@@ -2,7 +2,7 @@ const eventHub = document.querySelector(".container");
 const contentTarget = document.querySelector(".addFriend");
 
 const render = () => {
-  contentTarget.innerHTML = `<input type="text">Enter Friend to Save</input>`;
+  contentTarget.innerHTML = `<input id="newFriendEntry" type="text">Enter Friend to Save</input>`;
 };
 
 export const NewFriendEntry = () => {
--- src/scripts/friends/SaveFriendButton.js
+++ src/scripts/friends/SaveFriendButton.js
@@ -1,17 +1,33 @@
+import { useCurrentUser } from "../auth/LoginForm.js";
+import { useUsers, getUsers } from "../auth/UsersDataProvider.js";
+
 const eventHub = document.querySelector(".container");
 const contentTarget = document.querySelector(".saveFriend");
+const currentUser = useCurrentUser();
+let users = [];
 
 const render = () => {
   contentTarget.innerHTML = `<button class="" id="saveFriendButton" value="">Save New Friend</button>`;
 };
 
 export const SaveFriendButton = () => {
-  render();
+  getUsers()
+    .then(useUsers)
+    .then(() => {
+      users = useUsers();
+      render();
+    });
 };
 
 eventHub.addEventListener("click", (clickEvent) => {
-  if (clickEvent.target.id === "addFriendButton") {
-    const addFriendButtonEvent = new CustomEvent("saveFriendButtonClicked");
-    eventHub.dispatchEvent(addFriendButtonEvent);
+  if (clickEvent.target.id === "saveFriendButton") {
+    users = useUsers();
+    const friendName = document.querySelector("#newFriendEntry").value;
+    const matchingFriend = users.find((userObj) => {
+      return userObj.username === friendName;
+    });
+    debugger;
+    console.log(matchingFriend);
+    console.log(currentUser);
   }
 });
