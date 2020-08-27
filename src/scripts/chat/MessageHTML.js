98432d8b1da848cb84af156dc8ff896cd6071a8f
Gavin Schriver <alexanderschriver@gmail.com>
2020-08-27 17:57:58 -0500

    improving chat messages HTML structure

--- src/scripts/chat/MessageHTML.js
+++ src/scripts/chat/MessageHTML.js
@@ -1,18 +1,27 @@
 export const MessageHTML = (messageObj, currentUserId) => {
   if (currentUserId !== messageObj.user.id) {
-    return `<div class="message">
-            ${messageObj.user.username}
+    return `<div class="message nonActiveUser__message" id="message--${messageObj.id}">
+    
+            <div class="message__author" id="messageAuthorId--${messageObj.user.id}--messageId--${messageObj.id}">
+            ${messageObj.user.username}:
             </div>
-            <div>${messageObj.message}</div>
-            <div>${currentUserId}</div>
+
+            <div class="message__message" id="textForMessageId--${messageObj.id}">${messageObj.message}</div>
+
+            </div>
     `;
   } else {
-    return `<div class="message">
-            ${messageObj.user.username}
+    return `<div class="message activeUser__message" id="message--${messageObj.id}">
+            
+            <div class="message__author" id="messageAuthorId--${messageObj.user.id}--messageId--${messageObj.id}">
+            ${messageObj.user.username}:
             </div>
-            <div>${messageObj.message}</div>
-            <div>${currentUserId}</div>
-            <button id="deleteMessage--${messageObj.id}">Delete Message</button>
+
+            <div class="message__message" id="textForMessageId--${messageObj.id}">${messageObj.message}</div>
+            
+            <button class="button delete__button" id="deleteMessage--${messageObj.id}">Delete Message</button>
+
+            </div>
     `;
   }
 };
