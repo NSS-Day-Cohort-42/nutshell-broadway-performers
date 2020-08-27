b95b03ca912c87e3c25b0904f87dba1044582bc5
Gavin Schriver <alexanderschriver@gmail.com>
2020-08-27 15:43:53 -0500

    delete button SHOULDD only show up for messages from current user

--- src/scripts/chat/MessageHTML.js
+++ src/scripts/chat/MessageHTML.js
@@ -5,6 +5,13 @@ export const MessageHTML = (messageObj, currentUserId) => {
             </div>
             <div>${messageObj.message}</div>
             <div>${currentUserId}</div>
+    `;
+  } else {
+    return `<div class="message">
+            ${messageObj.user.username}
+            </div>
+            <div>${messageObj.message}</div>
+            <div>${currentUserId}</div>
             <button id="deleteMessage--${messageObj.id}">Delete Message</button>
     `;
   }
