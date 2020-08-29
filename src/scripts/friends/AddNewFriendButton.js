facee55b0c0e623ede0665a0a988241403fe3a87
Gavin Schriver <alexanderschriver@gmail.com>
2020-08-29 03:50:01 -0500

    weird toggle thing lol

--- src/scripts/friends/AddNewFriendButton.js
+++ src/scripts/friends/AddNewFriendButton.js
@@ -9,8 +9,12 @@ export const AddNewFriendButton = () => {
   render();
 };
 
+export const ShowFriendButton = () => {
+  return `<button class="" id="addFriendButton" value="">Add a Friend</button>`
+}
+
 eventHub.addEventListener("click", (clickEvent) => {
-  if (clickEvent.target.id === "addFriendButton") {
+  if (clickEvent.target.id === "addFriendButton" || clickEvent.target.id === "showFriendButton") {
     const addFriendButtonEvent = new CustomEvent("addNewFriendButtonClicked");
     eventHub.dispatchEvent(addFriendButtonEvent);
   }
--- src/scripts/friends/NewFriendEntry.js
+++ src/scripts/friends/NewFriendEntry.js
@@ -1,10 +1,19 @@
+import { AddNewFriendButton, ShowFriendButton } from "./AddNewFriendButton.js";
+
 const eventHub = document.querySelector(".container");
 const contentTarget = document.querySelector(".addFriend");
 
 const render = () => {
-  contentTarget.innerHTML = `<input id="newFriendEntry" type="text">Enter Friend to Save</input>`;
+  contentTarget.innerHTML = `<input id="newFriendEntry" type="text">Enter Friend to Save</input>
+                              <button id="closeNewFriendEntry">Close</button>`;
 };
 
 export const NewFriendEntry = () => {
   render();
 };
+
+eventHub.addEventListener("click", clickEvent => {
+  if (clickEvent.target.id === "closeNewFriendEntry") {
+    contentTarget.innerHTML = ShowFriendButton()
+  }
+})
