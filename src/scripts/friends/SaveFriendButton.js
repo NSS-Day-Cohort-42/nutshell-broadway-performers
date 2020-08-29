3ca109766640e606dd3795658d12487fdc61c182
Gavin Schriver <alexanderschriver@gmail.com>
2020-08-29 14:21:34 -0500

    data check for existing friendship in add new friend entry field

--- src/scripts/friends/SaveFriendButton.js
+++ src/scripts/friends/SaveFriendButton.js
@@ -1,11 +1,13 @@
 import { useCurrentUser } from "../auth/LoginForm.js";
 import { useUsers, getUsers } from "../auth/UsersDataProvider.js";
-import { saveFriend } from "./FriendsProvider.js";
+import { saveFriend, useFriends, getFriends } from "./FriendsProvider.js";
 
 const eventHub = document.querySelector(".container");
 const contentTarget = document.querySelector(".saveFriend");
 let currentUser;
 let users = [];
+let friends = [];
+let currentFriendsIds = [];
 
 const render = () => {
   contentTarget.innerHTML = `<button class="" id="saveFriendButton" value="">Save New Friend</button>`;
@@ -14,25 +16,34 @@ const render = () => {
 export const SaveFriendButton = () => {
   getUsers()
     .then(useUsers)
+    .then(getFriends)
     .then(() => {
       users = useUsers();
+      friends = useFriends();
       render();
     });
 };
 
 eventHub.addEventListener("click", (clickEvent) => {
   if (clickEvent.target.id === "saveFriendButton") {
+    friends = useFriends();
     currentUser = useCurrentUser();
     users = useUsers();
+    currentFriendsIds = (friends.filter(friendObj => {
+      return friendObj.userId === currentUser
+    })).map(friendObj => {
+      return friendObj.following
+    })
     const friendName = document.querySelector("#newFriendEntry").value;
     const matchingFriend = users.find((userObj) => {
       return userObj.username === friendName;
     });
-    const newFriendObj = {
-      userId: currentUser,
-      following: matchingFriend.id,
-    };
-
-    saveFriend(newFriendObj);
+    if (!currentFriendsIds.includes(matchingFriend.id)) {
+      const newFriendObj = {
+        userId: currentUser,
+        following: matchingFriend.id,
+      };
+      saveFriend(newFriendObj);
+    } else alert("already friends!")
   }
 });
