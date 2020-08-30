2cc3abfb811f927ef2857fa74854b70191517300
Gavin Schriver <alexanderschriver@gmail.com>
2020-08-30 18:17:47 -0500

    workin on some @ stuff

--- src/scripts/chat/MessageEntryForm.js
+++ src/scripts/chat/MessageEntryForm.js
@@ -1,16 +1,27 @@
 import { useCurrentUser } from "../auth/LoginForm.js";
 import { saveMessage } from "./MessagesProvider.js";
+import { getFriends, useFriends } from "../friends/FriendsProvider.js";
 const eventHub = document.querySelector(".container");
 const contentTarget = document.querySelector(".messageEntryForm");
 
 let currentUser; 
+let matchingFriends = []
+let matchingFriendsIds = []
+let matchingFriendsUsernames = []
 
 export const messageEntryForm = () => {
     currentUser = useCurrentUser()
-
-    render()
+    getFriends()
+        .then(() => {
+            matchingFriends = useFriends().filter(friendObj => {
+                return (friendObj.userId === currentUser)
+            })
+            matchingFriendsIds = matchingFriends.map(MFO => {
+                return (MFO.following)
+            })  
+            render()
+        })
 }
-
 
 const render = () => {
     contentTarget.innerHTML = `
@@ -24,13 +35,20 @@ const render = () => {
 
 eventHub.addEventListener("click", clickEvent => {
     if (clickEvent.target.id === "submitMessage") {
-        const newMessage = {
-            userId: currentUser,
-            message: document.querySelector("#messageText").value,
-            message_time: new Date()
+        if (document.querySelector("#messageText").value.startsWith("@")) {
+            alert('whew')
         }
 
-        saveMessage(newMessage)
+        else {
+            const newMessage = {
+                userId: currentUser,
+                message: document.querySelector("#messageText").value,
+                message_time: new Date()
+            }
+            saveMessage(newMessage)
+        }
+
+
     }
 })
 
