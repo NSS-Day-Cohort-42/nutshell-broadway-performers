b93fcc886166bd7a37ca6a4884bac1a6fed6a486
Michael-Tyler <miketylerabc@gmail.com>
2020-08-31 15:34:10 -0500

    commit to merge master

--- src/scripts/tasks/TaskHTML.js
+++ src/scripts/tasks/TaskHTML.js
@@ -3,11 +3,9 @@ export const noteHTMLConverter = (taskObj) => {
         return `
         <section id="task--${taskObj.id}" class="note">
         <div class="name">${taskObj.name}<input id="eventComplete--${taskObj.id}" class="showing" type="checkbox" /></div> 
-        <div class="date"><strong>ETA:</strong> ${new Date(taskObj.date).toLocaleDateString('en-US')}</div>
+        <div class="date"><strong>ETA:</strong> ${new Date(taskObj.date + 86400000).toLocaleDateString('en-US')}</div>
         <button id="noteBtn--${taskObj.id}">Delete</button>
         </section>
     `
     }
 }
-
-
