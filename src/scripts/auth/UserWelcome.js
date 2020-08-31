d81b9ae09a0245359b353371480425d2c18ab4ab
Gavin Schriver <alexanderschriver@gmail.com>
2020-08-31 15:44:51 -0500

    there we go

--- src/scripts/auth/UserWelcome.js
+++ src/scripts/auth/UserWelcome.js
@@ -2,14 +2,13 @@ import { useCurrentUser } from "./LoginForm.js";
 import { getUsers, useUsers } from "./UsersDataProvider.js";
 
 const contentTarget = document.querySelector(".userWelcome");
-let currentUsername = "";
+let currentUsername;
 
 export const UserWelcome = () => {
     getUsers()
         .then(useCurrentUser)
         .then(() => {
-            currentUsername = (useUsers().find(userObj => { return userObj.id === useCurrentUser()})).currentUsername
-            
+            currentUsername = (useUsers().find(userObj => userObj.id === useCurrentUser())).username
             render()
         })
 }
